3d3e4327991d09f23b15bae839242cb5ead4ada9
Badr aldin Alzein <badraldinalzein@mbp-von-badr.speedport.ip>
2022-03-14 20:27:52 +0100

    unittesting

---
+++ src/components/__tests__/themeModelToggle.test.js
@@ -0,0 +1,48 @@
+import ThemeModeToggle from "./../themeModeToggle";
+import configureStore from 'redux-mock-store';
+import * as CONST from "./../../constants";
+import { Provider, useDispatch } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { cleanup } from "@testing-library/react";
+import * as themeActions from "./../../actions/theme";
+const mockStore = configureStore([]);
+
+describe('themeModel Toggle tests', () => {
+ let store;
+ let component;
+ let themeButton;
+  beforeEach(() => {
+    store = mockStore({
+      theme:{
+          type: CONST.DARK,
+      }
+    });
+    component = renderer.create(
+      <Provider store={store}>
+        <ThemeModeToggle />
+      </Provider>
+    );
+    themeButton= component.root.findByType('button');
+    store.dispatch = jest.fn();
+    });
+    afterEach(()=>{
+        cleanup()
+    })
+    it('renderes correctly',() => {
+        expect(component).toBeDefined();
+        expect(themeButton).toBeDefined();
+    })
+    it('match snapshot',() => {
+        expect(component.toJSON()).toMatchSnapshot();
+    })
+    it('button renders correctly with dark mode',() => {
+        expect(themeButton._fiber.stateNode.children[0].text).toBe("🔆");
+    })
+    it('button works correctly with clicking',() => {  
+        renderer.act(() => {
+            themeButton.props.onClick();
+        });
+        expect(store.getActions()[0].type).toBe(CONST.THEME);
+        expect(store.getActions()[0].payload.type).toBe(CONST.LIGHT);
+    })
+})
